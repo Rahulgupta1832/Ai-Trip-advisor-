ec40304fd526efbacfd740057f69fcbd3687f76a
Rahul Kumar <158593245+Rahulgupta1832@users.noreply.github.com>
2025-12-01 00:43:34 +0530

    Delete CrewAI_tutorials-main/L-2/streamlit_trip_advisor_app/crewai.py

--- CrewAI_tutorials-main/L-2/streamlit_trip_advisor_app/crewai.py
+++
@@ -1,8 +0,0 @@
-# crewai.py (stub)
-class Agent:
-    def __init__(self, *args, **kwargs):
-        self.args = args
-        self.kwargs = kwargs
-    def run(self, *args, **kwargs):
-        return "CrewAI not available (stub)."
-__all__ = ["Agent"]
