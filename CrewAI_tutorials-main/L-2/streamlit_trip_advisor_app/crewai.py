ca79fa740746eb83e108d5893fe86ed3d572acf6
Rahul Kumar <158593245+Rahulgupta1832@users.noreply.github.com>
2025-12-01 00:30:03 +0530

    crewai.py

---
+++ CrewAI_tutorials-main/L-2/streamlit_trip_advisor_app/crewai.py
@@ -0,0 +1,8 @@
+# crewai.py (stub)
+class Agent:
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+    def run(self, *args, **kwargs):
+        return "CrewAI not available (stub)."
+__all__ = ["Agent"]
